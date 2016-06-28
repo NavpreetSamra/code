98fb4a1cd031318af1c3516c9bc449caabd9da7d
Mark Weiss <markweiss@Marks-MacBook-Pro.local>
2016-06-28 13:11:55 -0700

    sir owner -> model + log structure

--- sirdisease/sir_disease/__init__.py
+++ sirdisease/sir_disease/__init__.py
@@ -1,4 +1,5 @@
-import owner
+#import owner
+import model
 import subject
 
 __version__ = "0.0.1"
--- sirdisease/sir_disease/model.py
+++ sirdisease/sir_disease/model.py
@@ -1,29 +1,38 @@
 from scipy import spatial as spl
 import scipy as sp
 import numpy as np
-import subject
-#import h5infect
 from matplotlib import pyplot as plt
-
-#Metrics infection rate: density vs time, clusters(size + shape), critical saturation
-#quantifiable impact of vaccination
+import subject
 
 
-class kd_infect(spl.kdtree.KDTree):
-    '''
-    '''
-    def __init__(self, members=1000, size=(1, 1), infect_points=1, dist_type='uniform',
-             infect_radius=.001, infect_pct=.01, susceptible_pct=.85, duration=2, time=0,
-             record_name=None, local_path=None, group_name='default'):
-        '''
-        '''
+class kd_sir(spl.kdtree.KDTree):
+    """
+    Class for modeling SIR infectious disease
+
+    :param int members: initial population size
+    :param tuple size: (1x2) cell dimensions
+    :param int infect_points: number of initially infected members
+    :param str dist_type: distribution of members in cell
+    :param float infect_radius: distance constraint a member can infect\
+            another member
+    :param float infect_pct: percent chance an interaction leads to infection
+    :param float susceptible_pct: percent chance an individual is not immune
+    :param int duration: duration of infection
+    :param int time: initial day of model
+    :param sirlog record: object to handle recording simulation data
+
+    """
+    def __init__(self, members=1000, size=(1, 1), infect_points=1,
+                 dist_type='uniform', infect_radius=.001, infect_pct=.01,
+                 susceptible_pct=.85, duration=2, time=0, record=None):
+
         if dist_type == 'uniform':
             xs = size[0] * sp.rand(members, 1)
             ys = size[1] * sp.rand(members, 1)
 
         coordinates = np.c_[xs, ys]
 
-        #BCs
+        # BCs
         spl.kdtree.KDTree.__init__(self, coordinates)
         self.infect_points = infect_points
         self.infect_radius = infect_radius
@@ -33,45 +42,42 @@ class kd_infect(spl.kdtree.KDTree):
         self.subjects = {}
         self.time = time
 
-        #Records
+        # Records
         self.infected = None
         self.innoculated = None
         self.recovered = None
         self.infected_pct = infect_points / float(members)
         self.innoculated_pct = None
         self.recovered_pct = None
-        
-        #RecordKeeping
-        self.local_path = local_path
-        if group_name:
-            self.group_name = group_name
-        #Geometry
+
+        # Geometry
         self.area = size[0] * size[1]
-        self.rho  = members / self.area
+        self.rho = members / self.area
         self.radius = infect_radius / self.area
         self.infect_damage = self.radius * infect_pct
 
-        #PInumbers/metrics
+        # PInumbers/metrics
         self.infect_damage_rho = self.infect_damage * self.rho
 
-        if record_name:
-            self.record = h5infect.InfectFile(record_name)
+        self.record = record
 
     def init_infect(self):
-        '''
-        '''
+        """
+        Initialize members with attributes
+        """
         self.infected = np.zeros((self.data.shape[0]), dtype=bool)
         self.innoculated = np.zeros((self.data.shape[0]), dtype=bool)
         self.infected[0: self.infect_points] = True
 
         for i, j in zip(self.data, self.infected):
-            susceptible_seed = bool(rnum() < self.susceptible_pct)
+            susceptible_seed = bool(np.random.rand(1) < self.susceptible_pct)
             self.subjects[tuple(i)] = subject.Subject(i, susceptible_seed,
-                                               j, self.duration)
+                                                      j, self.duration)
 
     def step_time(self):
-        '''
-        '''
+        """
+        Step forward 1 cycle
+        """
 
         infected_index = []
         innoculated_index = []
@@ -84,7 +90,7 @@ class kd_infect(spl.kdtree.KDTree):
 
                 if neighbors_index:
                     for n in self.infected[neighbors_index]:
-                        if self.infect_pct > rnum():
+                        if self.infect_pct > np.random.rand(1):
                             infected_index.append(j)
                             self.subjects[sid].infect()
                             break
@@ -102,56 +108,27 @@ class kd_infect(spl.kdtree.KDTree):
             self.infected[np.array(innoculated_index)] = False
             self.innoculated[np.array(innoculated_index)] = True
 
-        if self.record_name:
-            self.record_step(self.record_name, self.local_path, self.group_name)
+        if self.record:
+            if self.time % self.record.frequency == 0:
+                self.record.record_step()
 
-    def record_step(self, file_path, local_path, group_name):
-        '''
-        '''
+        self.time += 1
 
-        self.format_record()
-        self.record.create_group(local_path)
-        self.record[local_path].create_dataset('data', data=self.data)
-        self.record[local_path].create_dataset('infected', data=self.infected)
-        self.record[local_path].create_dataset('innoculated', data=self.innoculated)
-
-
-    def format_record(self):
-        '''
-        '''
-        self.create_empty_record()
-
-
-    def create_empty_write_record(self):
+    def plot_step(self):
         """
+        Plot current state
         """
-        self.write_record = np.array(
-                                     tuple([' '] * len(self.fields)),
-                                         dtype=self.formats
-                                    )
-
-    def plot_step(self, file_path):
-        '''
-        '''
         x = self.data[:, 0]
         y = self.data[:, 1]
         plt.plot(x, y, 'k.', label='population')
-        plt.plot(x[self.infected], y[self.infected], 'r*', markersize=15, label='infected')
-        plt.plot(x[self.innoculated], y[self.innoculated], 'g*', markersize=15, label='innoculated')
-        plt.savefig(file_path)
+        plt.plot(x[self.infected], y[self.infected], 'r*', markersize=15,
+                 label='infected')
+        plt.plot(x[self.innoculated], y[self.innoculated], 'g*', markersize=15,
+                 label='innoculated')
+        if self.record:
+            plt.savefig(self.record.name + str(self.time))
+        else:
+            plt.savefig(str(self.time))
         plt.close()
         print(sum(self.infected))
         print(sum(self.innoculated))
-
-
-
-
-def rnum():
-    '''
-    '''
-    return np.random.rand(1)
-
-if __name__ == 'main':
-    tree = tree = owner.kd_infect()
-    tree.init_infect()
-
