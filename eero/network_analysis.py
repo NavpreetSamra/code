36a3747053c9d52b703eadc08a0a8702c5468fcc
Mark Weiss <markweiss@Marks-MacBook-Pro.local>
2016-07-22 16:23:01 -0700

    add graph build/analysis

--- eero/network_analysis.py
+++ eero/network_analysis.py
@@ -1,5 +1,8 @@
 import sys
 import numpy as np
+import itertools as it
+import networkx as nx
+import json
 sys.setrecursionlimit(1000)
 
 
@@ -24,6 +27,8 @@ class SystemNetwork(object):
         self.networks = {}
         self.nodes = {i:[] for i in self.node_list}
         self.nodeLinks = {}
+        self.graphs = {}
+        self.results = {}
 
         self._seed = self.node_list[0]
         self.networks = {0: list([self._seed])}
@@ -31,6 +36,7 @@ class SystemNetwork(object):
   
         self.macs_to_nodes()
         self.partition_system(self._seed)
+        self.eval_networks()
 
     def macs_to_nodes(self):
         """
@@ -73,23 +79,22 @@ class SystemNetwork(object):
             for i in nodes:
                 if i not in self.networks[index]:
                     new_nodes.extend(list([i]))
-            # new_nodes = [i for i in nodes if i not in self.networks[index]]
             if any(new_nodes):
                 self.networks[index].extend(new_nodes)
                 self._nodeTrack = self._nodeTrack[np.in1d(self._nodeTrack, np.asarray(new_nodes), invert=True)]
                 self.partition_system(new_nodes, index)
             elif any(self._nodeTrack):
-                index+=1
+                index += 1
                 seed = self._nodeTrack[0]
                 self.networks[index] = list([seed])
-                if len(self._nodeTrack) > 1 :
+                if len(self._nodeTrack) > 1:
                     self._nodeTrack = self._nodeTrack[1:]
                 else:
                     self._nodeTrack = np.array([])
                 self.partition_system(seed, index)
 
         elif any(self._nodeTrack):
-            index+=1
+            index += 1
             seed = self._nodeTrack[0]
             self.networks[index] = list([seed])
             if len(self._nodeTrack) > 1:
@@ -97,3 +102,51 @@ class SystemNetwork(object):
             else:
                 self._nodeTrack = np.array([])
             self.partition_system(seed, index)
+    
+    def eval_networks(self):
+        """
+        Evaluate quality of networks found in partition_system
+        """
+        for i in self.networks:
+            ntw = self.networks[i]
+            if len(ntw) > 1:
+                self.results[i] = self.build_graph(i, ntw)
+            else:
+                self.results[i] = 'Single node network: has no Peers'
+
+    def build_graph(self, index, netwrk):
+        """
+        Build directed graph for a network
+
+        :param array-like netwrk: array of nodes in network
+        :param int index: index of network in system
+        :return connected: whether or not all nodes are connected to all other nodes with quality connections
+        :rtype: bool
+        """
+        self.graphs[index] = nx.DiGraph()
+        connections = it.permutations(netwrk, 2)
+        for i in connections:
+            if i in self.nodeLinks:
+                if self.nodeLinks[i]:
+                    self.graphs[index].add_edge(i[0], i[1])
+        connected = True
+        for i in it.permutations(netwrk, 2):
+            if i[0] not in self.graphs[index] or i[1] not in self.graphs[index]:
+                connected = False
+                break
+            elif not nx.has_path(self.graphs[index], i[0], i[1]):
+                connected = False
+                break
+        return connected
+
+    def create_json(self, fname='analysis.json'):
+        out = {}
+        for i in self.networks:
+            d = {}
+            d['Connected'] = self.results[i]
+            d['Network'] = self.networks[i]
+            out[i] = d
+        outjson = open(fname, 'w')
+        j = json.dumps(out, indent=4)
+        print >> outjson, j
+        outjson.close()
