2e5686c7f882a1dc7e02f4d2277505744ac782f4
Mark Weiss <mark.weiss.mof@gmail.com>
2017-03-18 23:09:09 -0400

    add tokenizer type option

--- wordcounts/src/wordcounts.py
+++ wordcounts/src/wordcounts.py
@@ -1,8 +1,6 @@
 from abc import ABCMeta, abstractmethod
 import warnings
 import nltk
-import requests
-from collections import Counter
 
 
 class WordCounts(object):
@@ -52,16 +50,17 @@ class WordCounts(object):
         words and associated values
 
         :param int n: number to return, default None returns all
-        :return list tuple: key value pairs in sorted order
+        :return most_common: key value pairs in sorted order
+        :rtype: list.tuple.(str, int)
         """
         warnings.warn('attempted abstract counter')
 
     @abstractmethod
     def _auto(self):
-        warnings.warn('attempted abstract parse')
+        warnings.warn('attempted abstract _auto')
 
     @staticmethod
-    def word_only_tokenize(txt):
+    def words_tokenizer(txt):
         """
         Tokenize with nltk removing punctuations
 
@@ -73,9 +72,9 @@ class WordCounts(object):
                 nltk.wordpunct_tokenize(txt) if word.isalpha()]
 
     @staticmethod
-    def contraction_tokenize(txt):
+    def contraction_tokenizer(txt):
         """
-        Remove epostraphes (it's -> its) and tokenize with nltk
+        Remove apostraphes (it's -> its) and tokenize with nltk
         removing punctuations
 
         :param str txt: text to toknize
@@ -83,57 +82,4 @@ class WordCounts(object):
         :rtype: list
         """
         txt.replace("'", "")
-        return WordCounts.word_only_tokenize(txt)
-
-
-class WordCountsSerial(WordCounts):
-    """
-    Word frequency with :py:class:`collections.Counter` with file (text)
-    and html reading interfaces
-
-    :param str fPath: path to file
-    :param str parserType: type of file (html | file)
-    :param list wordList: option to specify words to count without parsing
-                          from file
-    :param bool auto: auto run
-    """
-    def __init__(self, fPath=None, parserType='html',
-                 wordList=None, auto=True):
-        self.fPath = fPath
-        self.parserType = parserType
-
-        self.text = []
-        if wordList:
-            self.text = wordList
-
-        if auto:
-            self._auto()
-
-    def parser(self, fPath, parserType):
-        """
-        Front end for parsing
-
-        :param str fPath: path to file
-        :param str parserType: type of file (html | file)
-        """
-        self.__getattribute__('_' + parserType + '_parser')(fPath)
-
-    def _html_parser(self, fPath):
-        raw = requests.get(fPath).text
-        self.text = self.word_only_tokenize(raw)
-
-    def _file_parser(self, fPath):
-        with open(fPath) as f:
-            for line in f:
-                self.text.extend(self.word_only_tokenize(line))
-
-    def counter(self):
-        self.wordCounts = Counter(self.text)
-
-    def most_common(self, n=None):
-        return self.wordCounts.most_common(n)
-
-    def _auto(self):
-        if self.fPath:
-            self.parser(self.fPath, self.parserType)
-        self.counter()
+        return WordCounts.words_tokenizer(txt)
